753bbbb9f0c6c7435e2b70ab3e3bd7a699164b7a
Saka-CS <hmmlm78@gmail.com>
2025-03-26 16:57:58 +0300

    Finish 0 to 3 exercises

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,7 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    array = str.split("")
+    array.reverse()
+    return array.join("")
 };
 
 // Do not edit below this line
