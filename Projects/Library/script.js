86170fca806539d0ef45956be8df7f126b44fa6a
Saka <hmmlm78@gmail.com>
2025-09-15 09:23:02 +0300

    .

--- Projects/Library/script.js
+++ Projects/Library/script.js
@@ -4,7 +4,19 @@ const myLibrary = [];
 function Book() {
     this.id = crypto.randomUUID()
 }
-
+                // <div class="book">
+                //     <div class="one-line">
+                //         <h3>Title</h3>
+                //         <h4>.    -</h4>
+                //         <h4>Author</h4>
+                //     </div>
+                //     <h4>234 Pages</h4>
+                //     <h4>Completed</h4>
+                //     <h4>Mark As</h4>
+                //     <button>Reading</button>
+                //     <button>Queued</button>
+                //     <button>Completed</button>
+                // </div>
 function addBookToLibrary() {
     
 }
