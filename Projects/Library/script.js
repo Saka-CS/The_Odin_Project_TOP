625a7c5d4a3d490cb2cadc664425dd2af42da5eb
Saka <hmmlm78@gmail.com>
2025-09-19 09:17:58 +0300

    Complete book addition function

--- Projects/Library/script.js
+++ Projects/Library/script.js
@@ -1,9 +1,12 @@
 
-const myLibrary = [];
+const readingLibrary = [];
+const queuedLibrary = [];
+const completedLibrary = [];
 
-function Book() {
-    this.id = crypto.randomUUID()
-}
+const readingBooks = document.querySelector('#reading');
+const queuedBooks = document.querySelector('#queued');
+const completedBooks = document.querySelector('#completed');
+
                 // <div class="book">
                 //     <div class="one-line">
                 //         <h3>Title</h3>
@@ -17,6 +20,64 @@ function Book() {
                 //     <button>Queued</button>
                 //     <button>Completed</button>
                 // </div>
-function addBookToLibrary() {
-    
+
+
+function Book(title, author, numPages, states) {
+    this.id = crypto.randomUUID();
+    this.title = title;
+    this.author = author;
+    this.numPages = numPages;
+    this.states = states;
 }
+
+function addBookToReadingLibrary(title, author, numPages, states) {
+    readingLibrary.push(new Book(title, author, numPages, states));
+    bookInformation = readingLibrary[readingLibrary.length - 1];
+    console.log(readingLibrary);
+
+    newBook = document.createElement('div');
+    newBook.classList.add('book');
+
+    titleDiv = document.createElement('div');
+    titleDiv.classList.add('one-line');
+
+    titleDiv.innerHTML = `  <h3>${bookInformation.title}</h3>
+                            <h4><pre>    -${bookInformation.author}</h4>`;
+    newBook.appendChild(titleDiv);
+
+    newBook.innerHTML += `  <h4>${bookInformation.numPages} Pages</h4>
+                            <h4>${bookInformation.states}</h4>
+                            <h4>Mark As</h4>
+                            <button class='testing'>Reading</button>
+                            <button>Queued</button>
+                            <button>Completed</button>
+                            `;
+
+    // bookTitle = document.createElement('h3');
+    // bookTitle.textContent = bookInformation.title;
+    // titleDiv.appendChild(bookTitle);
+
+    // bookAuthor = document.createElement('h4');
+    // bookAuthor.innerHTML = `<pre>  -${bookInformation.author}`;
+    // titleDiv.appendChild(bookAuthor);
+    // newBook.appendChild(titleDiv);
+
+
+    // bookPages = document.createElement('h4');
+    // bookPages.textContent = bookInformation.numPages + ' Pages';
+    // newBook.appendChild(bookPages);
+
+    // bookStatus = document.createElement('h4');
+    // bookStatus.textContent = bookInformation.states;
+    // newBook.appendChild(bookStatus);
+
+
+
+    readingBooks.appendChild(newBook);
+    console.log(newBook);
+
+}
+
+addBookToReadingLibrary('The Art of the Craft', '', 234, 'Reading');
+addBookToReadingLibrary('How to Build A Second Brain', 'Hassan', 234, 'Reading');
+addBookToReadingLibrary('The Art of the Craft', '', 234, 'Reading');
